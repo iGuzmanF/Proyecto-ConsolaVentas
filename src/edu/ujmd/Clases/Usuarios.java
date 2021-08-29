0ad188b66da81e418817268782ed536176ecc4e4
MarioE99 <meev99@hotmail.com>
2021-08-29 16:51:12 -0600

    Se ha agregado el login funcional

--- src/edu/ujmd/Clases/Usuarios.java
+++ src/edu/ujmd/Clases/Usuarios.java
@@ -10,68 +10,84 @@ package edu.ujmd.Clases;
  * @author meev9
  */
 public class Usuarios {
-    
 
-     //Declaramos las variables de acceso
-        
-          private String Usuario;
-        
-          private String Contraseña;
-        
-        //Declaramos las constantes para nuestro programa:Nivel,usuario y contraseña
-         private  int niveladmin=1;//Nivel del admin
-        
-         private String Admin = "admin";//Usuario Admin
-        
-         private String ContraseñaA ="admin123";//Contraseña del Admin
-        
-        //--------------------------------------------------
-        
-       private int nivelvendedor=2; //Nivel del vendedor
-        
-       private  String Vendedor="vendedor";//Usuario del vendedor
-        
-       private String ContraseñaV ="vendedor123";//Contraseña del vendedor
-        
-        //---------------------------------------------------------
-        
-       private int nivelinvitado=3;//Nivel del invitado
-        
-       private String Invitado="invitado";//Usuario del invitado
-        
-       private String ContraseñaI ="invitado123";//Contraseña del invitado
-        
-    
-    
-    public Usuarios()
-    {
-        
-        
+    //Declaramos las constantes para nuestro programa:usuario y contraseña
+    private String Admin = new String("admin");//Usuario Admin
+
+    private String ContraseñaA = new String("admin123");//Contraseña del Admin
+
+    //--------------------------------------------------
+    private String Vendedor = new String("vendedor");//Usuario del vendedor
+
+    private String ContraseñaV = new String("vendedor123");//Contraseña del vendedor
+
+    //---------------------------------------------------------
+    private String Invitado = new String("invitado");//Usuario del invitado
+
+    private String ContraseñaI = new String("invitado123");//Contraseña del invitado
+
+    public Usuarios() {
+
     }
+
+    /*
+     
+    LLUVIAS DE IDEAS
     
+    1. Cada usuario tendra una contraseña
+    
+    2. Tenemos que validar si el usuario ingresado es correcto
+    
+    3. Una vez el usuario este validado, ingresamos al menu
+    
+    
+    
+     */
     //En este metodo, validaremos si el usuario y contraseña ingresado es correcto
-    //(Aun no implementado)
-    public void AccesoConcecido( String us, String con)
-    {
-       
-        
-        if(Admin==us && ContraseñaA==con)
-        {
-            System.out.println("Acceso concedido");
+    
+    
+    public boolean AccesoConcecidoAdministrador(String us, String con) {
+
+        boolean paso=false;
+        if (Admin.equals(us) && ContraseñaA.equalsIgnoreCase(con)) {
+            System.out.println("Acceso concedido!!!");
+            paso=true;
+
+        } else {
+
+            System.out.println("Vuelva a iniciar las credenciales");
+            paso=false;
         }
         
-        else
-        {
-            System.out.println("Acceso denegado, vuelva a ingresar usuario o contraseña");
-            
-            
-        }
-        
-        
+        return paso;
+
     }
-    
-    
-    
-   
-    
+
+    public boolean AccesoConcedidoVendedor(String us, String con) {
+        
+        boolean paso2;
+        if (Vendedor.equals(us) && ContraseñaV.equalsIgnoreCase(con)) {
+            System.out.println("Acceso concedido!!!");
+            paso2=true;
+        } else {
+            System.out.println("Acceso denegado, vuelva a ingresar usuario o contraseña");
+            paso2=false;
+        }
+        
+        return paso2;
+    }
+
+    public boolean AccesoConcecidoInvitado(String us, String cont) {
+        boolean paso3;
+        if (Invitado.equals(us) && ContraseñaI.equalsIgnoreCase(cont)) {
+            System.out.println("Acceso concedido");
+            paso3=true;
+        } else {
+            System.out.println("Acceso denegado, vuelva a ingresar usuario o contraseña");
+            paso3=false;
+        }
+        
+        return paso3;
+    }
+
 }
--- src/edu/ujmd/consolaventas/ProyectoConsolaVentas.java
+++ src/edu/ujmd/consolaventas/ProyectoConsolaVentas.java
@@ -6,7 +6,6 @@
 package edu.ujmd.consolaventas;
 
 import edu.ujmd.Clases.Usuarios;
-import java.util.InputMismatchException;
 import java.util.Scanner;
 
 /**
@@ -25,83 +24,143 @@ public class ProyectoConsolaVentas {
 
         String cont;
 
+        int nivel;
+
+        //Declaramos el scanner
+        Scanner in = new Scanner(System.in);
+
         //Realizamos el Login para los usuarios
         System.out.println("Bienvenidos al sistema de ventas Don Diego");
 
-        System.out.println("Ingrese las credenciales");
-
-        Scanner in = new Scanner(System.in);
-
         //Realizamos el menu de acceso    
-        System.out.println("Ingrese el usuario ");
+        System.out.println("Ingrese el nivel de acceso del usuario");
+        System.out.println("Nivel 1 ->Administrador");
+        System.out.println("Nivel 2 ->Vendedor");
+        System.out.println("Nivel 3 ->Invitado");
 
-        usuario = in.nextLine();
+        nivel = in.nextInt();
 
-        System.out.println("Ingrese la contraseña");
+        //Creamos la istancia
+        Usuarios U = new Usuarios();
+        //Nivel de acceso del usuario
 
-        cont = in.nextLine();
+        //Realizamos un Switch para identificar los usuarios
+        switch (nivel) {
+            case 1:
+
+                do {
+                    System.out.println("Bienvenido administrador al sistema de ventas Don Jose");
+
+                    System.out.println("Ingrese el usuario ");
+
+                    usuario = in.next();
+
+                    System.out.println("Ingrese la contraseña");
+
+                    cont = in.next();
+
+                } while (U.AccesoConcecidoAdministrador(usuario, cont) == false);
+
+                break;
+
+            case 2:
+
+                do {
+
+                    System.out.println("Bienvenido vendedor al sistema de ventas Don Jose");
+
+                    System.out.println("Ingrese el usuario ");
+
+                    usuario = in.next();
+
+                    System.out.println("Ingrese la contraseña");
+
+                    cont = in.next();
+
+                } while (U.AccesoConcedidoVendedor(usuario, cont) == false);
+
+                break;
+
+            case 3:
+
+                do {
+
+                    System.out.println("Bienvenido invitado al sistema de ventas Don Jose");
+
+                    System.out.println("Ingrese el usuario ");
+
+                    usuario = in.next();
+
+                    System.out.println("Ingrese la contraseña");
+
+                    cont = in.next();
+
+                } while (U.AccesoConcecidoInvitado(usuario, cont) == false);
+
+                break;
+
+        }
 
         //Realizamos el menu del sistema de ventas
-        Scanner sn = new Scanner(System.in);
         boolean salir = false;
         int opc;
 
-        while (!salir) {
-            System.out.println("Menu de opciones");
+        //Declaramos el scanner
+        Scanner sn = new Scanner(System.in);
+        
+        do{
+            
+             System.out.println("Menu de opciones");
             System.out.println("1. Venta nueva");
             System.out.println("2. Consultar inventarios");
             System.out.println("3. Cambiar contraseña");
             System.out.println("4. Salir del programa");
 
-            try {
-                System.out.println("Seleccione la opcion que desee realizar");
-                opc = sn.nextInt();
+            System.out.println("Seleccione la opcion que desee realizar");
+            opc = sn.nextInt();
 
-                switch (opc) {
-                    case 1:
+            switch (opc) {
+                case 1:
 
-                        System.out.println("**Menu para realizar nueva venta**");
-                        System.out.println("Ingrese la nueva venta que desea realizar:");
-                        break;
+                    System.out.println("**Menu para realizar nueva venta**");
+                    System.out.println("Ingrese la nueva venta que desea realizar:");
+                    break;
 
-                    case 2:
-                        System.out.println("**Menu para ver la lista de inventarios**");
-                        System.out.println("La lista de los inventarios es la siguiente:");
-                        System.out.println("Catalogo de productos:");
-                        System.out.print("1- huevos\n"
-                                + "2- pollo\n"
-                                + "3- aceite\n"
-                                + "4- fósforos\n"
-                                + "5- dulces\n"
-                                + "6- margarina\n"
-                                + "7- jabón\n"
-                                + "8- carne\n"
-                                + "9- gaseosa\n"
-                                + "10- desechables\n");
-                        break;
+                case 2:
+                    System.out.println("**Menu para ver la lista de inventarios**");
+                    System.out.println("La lista de los inventarios es la siguiente:");
+                    System.out.println("Catalogo de productos:");
+                    System.out.print("1- huevos\n"
+                            + "2- pollo\n"
+                            + "3- aceite\n"
+                            + "4- fósforos\n"
+                            + "5- dulces\n"
+                            + "6- margarina\n"
+                            + "7- jabón\n"
+                            + "8- carne\n"
+                            + "9- gaseosa\n"
+                            + "10- desechables\n");
+                    break;
 
-                    case 3:
-                        System.out.println("**Menu para cambiar contraseña**");
-                        System.out.println("Seleccione su nueva contraseña:");
-                        System.out.println("Escriba nuevamente su contraseña nueva:");
-                        break;
+                case 3:
+                    System.out.println("**Menu para cambiar contraseña**");
+                    System.out.println("Seleccione su nueva contraseña:");
+                    System.out.println("Escriba nuevamente su contraseña nueva:");
+                    break;
 
-                    case 4:
-                        System.out.println("Saliendo del programa");
-                        salir = true;
-                        break;
+                case 4:
+                    System.out.println("Saliendo del programa");
+                    salir = true;
+                    break;
 
-                    default:
-                        System.out.println("Solo numeros entre 1 al 4");
-                        break;
-                }
-
-            } catch (InputMismatchException e) {
-                System.out.println("debes insertar un numero antes");
-                sn.next();
+                default:
+                    System.out.println("Solo numeros entre 1 al 4");
+                    break;
             }
-
-        }
+            
+        }while(opc!=4);
+       
 
     }
+
 }
