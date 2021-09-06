855a94bac2b77f8acab53e6385f08ab3791b4c90
MarioE99 <meev99@hotmail.com>
2021-09-05 23:27:49 -0600

    Se agrego la bitacora de acceso y mostrar inventario en pantalla

--- src/edu/ujmd/consolaventas/ProyectoConsolaVentas.java
+++ src/edu/ujmd/consolaventas/ProyectoConsolaVentas.java
@@ -5,7 +5,10 @@
  */
 package edu.ujmd.consolaventas;
 
+import edu.ujmd.Archivos.Archivos;
 import edu.ujmd.Clases.Usuarios;
+import java.time.LocalDateTime;
+import java.time.Month;
 import java.util.Scanner;
 
 /**
@@ -42,10 +45,14 @@ public class ProyectoConsolaVentas {
 
         //Creamos la istancia
         Usuarios U = new Usuarios();
-        //Nivel de acceso del usuario
+        //Creamos la isntancia de escribir y leer archivo
+
+        Archivos a = new Archivos();
 
         //Realizamos un Switch para identificar los usuarios
         switch (nivel) {
+
+            //Login del administrador
             case 1:
 
                 do {
@@ -60,9 +67,65 @@ public class ProyectoConsolaVentas {
                     cont = in.next();
 
                 } while (U.AccesoConcecidoAdministrador(usuario, cont) == false);
+                LocalDateTime localDate = LocalDateTime.now();
+                int hora = localDate.getHour();
+                int minuto = localDate.getMinute();
+                int segundo = localDate.getSecond();
+                
+                
+                int dia = localDate.getDayOfMonth();
+                int mes =localDate.getMonthValue();
+                int anio = localDate.getYear();
+               
+                a.EscribirFichero("\nAdministrador","\nHora:",hora,minuto,segundo,"\nFecha:",dia,mes,anio);
+
+                boolean salir = false;
+                int opc;
+                do {
+
+                    System.out.println("Digite la opcion a solicitar");
+                    System.out.println("1-Venta nueva");
+                    System.out.println("2-Consultar inventario");
+                    System.out.println("3-Cambiar contraseña");
+                    System.out.println("4-Salir del programa");
+                    System.out.println("Digite la opcion");
+                    opc = in.nextInt();
+                    switch (opc) {
+
+                        case 1:
+
+                            System.out.println("**Menu para realizar nueva venta**");
+                            System.out.println("Ingrese la nueva venta que desea realizar:");
+                            break;
+
+                        case 2:
+                            System.out.println("**Menu para ver la lista de inventarios**");
+                            System.out.println("La lista de los inventarios es la siguiente:");
+                            a.LeerFichero();
+                            break;
+
+                        case 3:
+                            System.out.println("**Menu para cambiar contraseña**");
+                            System.out.println("Seleccione su nueva contraseña:");
+                            System.out.println("Escriba nuevamente su contraseña nueva:");
+                            break;
+
+                        case 4:
+                            System.out.println("Saliendo del programa");
+                            a.EscribirFichero("\nSalida del Administrador","\nHora:",hora,minuto,segundo,"\nFecha:",dia,mes,anio);
+                            salir = true;
+                            break;
+
+                        default:
+                            System.out.println("Solo numeros entre 1 al 4");
+                            break;
+
+                    }
+
+                } while (opc != 4);
 
                 break;
-
+            //Login del vendedor
             case 2:
 
                 do {
@@ -79,8 +142,57 @@ public class ProyectoConsolaVentas {
 
                 } while (U.AccesoConcedidoVendedor(usuario, cont) == false);
 
+                LocalDateTime localDate2 = LocalDateTime.now();
+                int hora2 = localDate2.getHour();
+                int minuto2 = localDate2.getMinute();
+                int segundo2 = localDate2.getSecond();
+                
+                int dia2 = localDate2.getDayOfMonth();
+                int mes2 =localDate2.getMonthValue();
+                int anio2 = localDate2.getYear();
+               
+                a.EscribirFichero("\nVendedor","\nHora:",hora2,minuto2,segundo2,"\nFecha:",dia2,mes2,anio2);
+
+                int opc1;
+                do {
+
+                    System.out.println("Digite la opcion a solicitar");
+                    System.out.println("1-Venta nueva");
+                    System.out.println("2-Consultar inventario");
+                    System.out.println("3-Salir del programa");
+                    System.out.println("Digite la opcion");
+                    opc1 = in.nextInt();
+
+                    switch (opc1) {
+                        case 1:
+
+                            System.out.println("**Menu para realizar nueva venta**");
+                            System.out.println("Ingrese la nueva venta que desea realizar:");
+                            break;
+
+                        case 2:
+                            System.out.println("**Menu para ver la lista de inventarios**");
+                            System.out.println("La lista de los inventarios es la siguiente:");
+                            a.LeerFichero();
+                            break;
+
+                        case 3:
+                            System.out.println("Saliendo del programa");
+                            salir = true;
+                           a.EscribirFichero("\nSalida del Vendedor","\nHora:",hora2,minuto2,segundo2,"\nFecha:",dia2,mes2,anio2);
+                            break;
+
+                        default:
+                            System.out.println("Solo numeros entre 1 al 4");
+                            break;
+
+                    }
+
+                } while (opc1 != 3);
+
                 break;
 
+            //Modo invitado   
             case 3:
 
                 do {
@@ -97,69 +209,44 @@ public class ProyectoConsolaVentas {
 
                 } while (U.AccesoConcecidoInvitado(usuario, cont) == false);
 
+                LocalDateTime localDate3 = LocalDateTime.now();
+                int hora3 = localDate3.getHour();
+                int minuto3 = localDate3.getMinute();
+                int segundo3 = localDate3.getSecond();
+                
+                int dia3 = localDate3.getDayOfMonth();
+                int mes3 =localDate3.getMonthValue();
+                int anio3 = localDate3.getYear();
+                
+                a.EscribirFichero("\nInvitado","\nHora:",hora3,minuto3,segundo3,"\nFecha:",dia3,mes3,anio3);
+                int opc2;
+
+                do {
+                    System.out.println("Digite la opcion a solicitar");
+                    System.out.println("1-Consultar inventario");
+                    System.out.println("2-Salir del programa");
+                    opc2 = in.nextInt();
+                    switch (opc2) {
+
+                        case 1:
+                            System.out.println("**Menu para ver la lista de inventarios**");
+                            System.out.println("La lista de los inventarios es la siguiente:");
+                            a.LeerFichero();
+                            break;
+
+                        case 2:
+                            System.out.println("Saliendo del programa");
+                            salir = true;
+                            a.EscribirFichero("\nSalida del Invitado","\nHora:",hora3,minuto3,segundo3,"\nFecha:",dia3,mes3,anio3);
+                            break;
+
+                    }
+
+                } while (opc2 != 2);
+
                 break;
 
         }
-
-        //Realizamos el menu del sistema de ventas
-        boolean salir = false;
-        int opc;
-
-        //Declaramos el scanner
-        Scanner sn = new Scanner(System.in);
-        
-        do{
-            
-             System.out.println("Menu de opciones");
-            System.out.println("1. Venta nueva");
-            System.out.println("2. Consultar inventarios");
-            System.out.println("3. Cambiar contraseña");
-            System.out.println("4. Salir del programa");
-
-            System.out.println("Seleccione la opcion que desee realizar");
-            opc = sn.nextInt();
-
-            switch (opc) {
-                case 1:
-
-                    System.out.println("**Menu para realizar nueva venta**");
-                    System.out.println("Ingrese la nueva venta que desea realizar:");
-                    break;
-
-                case 2:
-                    System.out.println("**Menu para ver la lista de inventarios**");
-                    System.out.println("La lista de los inventarios es la siguiente:");
-                    System.out.println("Catalogo de productos:");
-                    System.out.print("1- huevos\n"
-                            + "2- pollo\n"
-                            + "3- aceite\n"
-                            + "4- fósforos\n"
-                            + "5- dulces\n"
-                            + "6- margarina\n"
-                            + "7- jabón\n"
-                            + "8- carne\n"
-                            + "9- gaseosa\n"
-                            + "10- desechables\n");
-                    break;
-
-                case 3:
-                    System.out.println("**Menu para cambiar contraseña**");
-                    System.out.println("Seleccione su nueva contraseña:");
-                    System.out.println("Escriba nuevamente su contraseña nueva:");
-                    break;
-
-                case 4:
-                    System.out.println("Saliendo del programa");
-                    salir = true;
-                    break;
-
-                default:
-                    System.out.println("Solo numeros entre 1 al 4");
-                    break;
-            }
-            
-        }while(opc!=4);
-       
 
     }
 
